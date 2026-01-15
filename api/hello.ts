2a4f2e532627067c9825b823cb5d32d1f34553e4
linktang <tomleader4444@gmail.com>
2026-01-15 20:19:57 +0800

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse111 } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({
